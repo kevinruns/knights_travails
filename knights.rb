c887449790ec5386a9724c8466e9810c54785ffe
Kevin Lenehan <klenehan123@gmail.com>
2020-11-16 08:45:09 +0000

    will remove function asking for target

--- knights.rb
+++ knights.rb
@@ -1,35 +1,28 @@
 # chess board
 class Board
-  attr_accessor :board
+  attr_accessor :board, :board_size
 
   def initialize
     @board_size = 8
     @board = Array.new(@board_size){Array.new(@board_size)}
-    @knight = Knight.new
   end
 
-  def move_piece
-    all_moves = @knight.moves
-    valid_moves = all_moves.select { |move| onboard(move) }
-    p valid_moves
+  def square_visited(coord)
+    return true if @board[rank, file] == 'x'
+    rank = coord[0]
+    file = coord[1]
+    @board[rank, file] = 'x'
   end
 
-  def onboard(coord)
-    coord[0].between?(0, @board_size - 1) && coord[1].between?(0, @board_size - 1)
-  end
+end
 
-  def target_square
-    good_input = false
-    target = ""
-    until good_input
-      print "Enter target square, rank & file e.g. 3 5 :  "
-      target = gets.chomp.split(" ").map(&:to_i)
-      good_input = onboard(target)
-    end
-    target
-  end
+# node
+class Node
 
-  def
+  def initialize
+    @root = [0,0]
+    @parent = nil
+  end
 
 end
 
@@ -38,11 +31,10 @@ class Knight
   attr_accessor :postiion
 
   def initialize
-    @start = [0, 0]
     @position = [0, 0]
   end
 
-  def moves(position = @start)
+  def all_moves(position = @position)
     moves = []
     moves[0] = [position[0] + 2,position[1] + 1]
     moves[1] = [position[0] + 2,position[1] - 1]
@@ -56,7 +48,53 @@ class Knight
   end
 end
 
-game = Board.new
-game.move_piece
-game.target_square
+class Game
+  
+  def initialize
+    @knight = Knight.new
+    @board = Board.new
+  end
+
+  def move_piece
+    all_moves = @knight.all_moves
+    valid_moves = all_moves.select { |move| onboard(move) }
+    p valid_moves
+  end
+
+  def onboard(coord)
+    coord[0].between?(0, @board.board_size - 1) && coord[1].between?(0, @board.board_size - 1)
+  end
+
+  def target_square
+    good_input = false
+    target = ""
+    until good_input
+      print "Enter target square, rank & file e.g. 3 5 :  "
+      target = gets.chomp.split(" ").map(&:to_i)
+      good_input = onboard(target)
+    end
+    target
+  end
+
+  def knight_moves(start, target)
+
+    node = Node.new
+    moves = move_piece
+
+    moves.each do |move|
+      if move == target
+        print "Congratulations target found \n"
+        str = " #{start} , #{move} \n" 
+        print str
+      else
+        node = Node.new()  # TO CONTINUE
+      end
+    end
+    
+  end
+
+end
+
+game = Game.new
+game.knight_moves([0,0],[1,2])
 
