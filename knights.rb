1654bb5f6cf8a27e4f54dac0678689cb86d39ad7
Kevin Lenehan <klenehan123@gmail.com>
2020-11-16 11:42:11 +0000

    need to process node layers sequentially, adding node array

--- knights.rb
+++ knights.rb
@@ -4,24 +4,37 @@ class Board
 
   def initialize
     @board_size = 8
-    @board = Array.new(@board_size){Array.new(@board_size)}
+    @board = Array.new(@board_size){Array.new(@board_size, '-')}
   end
 
   def square_visited(coord)
-    return true if @board[rank, file] == 'x'
     rank = coord[0]
     file = coord[1]
-    @board[rank, file] = 'x'
+    return true if @board[rank][file] == 'x'
+    @board[rank][file] = 'x'
+    return false
   end
 
+  def print_board
+    print "\n"
+    @board.each_with_index do |_row, i|
+      print '    '
+      @board[i].each_with_index do |_col, j|
+        print " #{@board[i][j]} "
+      end
+      print "\n"
+    end
+    print "\n"
+  end
 end
 
 # node
 class Node
+  attr_accessor :position, :parent
 
-  def initialize
-    @root = [0,0]
-    @parent = nil
+  def initialize(start = [0, 0], parent)
+    @position = start
+    @parent = parent
   end
 
 end
@@ -49,52 +62,80 @@ class Knight
 end
 
 class Game
-  
+
   def initialize
     @knight = Knight.new
     @board = Board.new
+    @start = [0,0]
+    @target = [0,0]
   end
 
-  def move_piece
-    all_moves = @knight.all_moves
-    valid_moves = all_moves.select { |move| onboard(move) }
-    p valid_moves
+  def move_piece(position)
+    all_moves = @knight.all_moves(position)
+    valid_moves = all_moves.select { |move| onboard(move) && !@board.square_visited(move) }
   end
 
   def onboard(coord)
     coord[0].between?(0, @board.board_size - 1) && coord[1].between?(0, @board.board_size - 1)
   end
 
-  def target_square
-    good_input = false
-    target = ""
-    until good_input
-      print "Enter target square, rank & file e.g. 3 5 :  "
-      target = gets.chomp.split(" ").map(&:to_i)
-      good_input = onboard(target)
-    end
-    target
+  def check_inputs(start, target)
+    onboard(start) && onboard(target) && start != target
   end
 
+  # main method for assigning start and target and initiating moves
   def knight_moves(start, target)
+    return 0 unless check_inputs(start, target)
+    @board.print_board
+    @start = start
+    @target = target
 
-    node = Node.new
-    moves = move_piece
+    node = Node.new(start, nil)
+    find_path(node)
 
-    moves.each do |move|
-      if move == target
+  end
+
+  def find_path(node)
+    moves = move_piece(node.position)
+    return if moves.length == 0
+    p moves
+# create node array? to process each turn????
+    moves.each do |new_position|
+      parent = node
+      node = Node.new(new_position, parent)
+      print "Node position #{node.position} \n"
+      print "Node parent position #{node.parent.position} \n"
+
+      if new_position == @target
         print "Congratulations target found \n"
-        str = " #{start} , #{move} \n" 
-        print str
+        print_path(node)
+        return
       else
-        node = Node.new()  # TO CONTINUE
+        print "Go deeper #{node.position}\n"
+        find_path(node)
       end
     end
-    
   end
 
+  def print_path(node)
+    str = ""
+    while node.parent
+      str.prepend(node.position.to_s)
+      node = node.parent
+      print "Path: #{str} \n"
+    end
+    print "Path: #{str} \n"
+  end
 end
 
+# board = Board.new
+# board.print_board
+
+# board.board[2][2] = 'x'
+# board.print_board
+
+# p board.square_visited([2,2])
+
 game = Game.new
-game.knight_moves([0,0],[1,2])
+game.knight_moves([0,0],[3,3])
 
